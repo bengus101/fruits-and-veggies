044559c38dcbef15a76483ed2658a7186277b36d
bengus101 <bgustafson29@yahoo.com>
2016-05-23 19:04:36 -0700

    Items now go into their proper columns.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,6 +1,31 @@
 var app = angular.module("MyApp", []);
 app.controller("HomeCtrl", ['$scope', function($scope) {
-	$scope.fruit
+	$scope.fruit = [];
+	$scope.fruits = [
+		"Apple",
+		"Apricot",
+		"Banana"
+	];
+
+	$scope.veggies = [];
+	$scope.vegetables = [
+		"Artichoke",
+		"Arugula",
+		"Asparagus"
+	];
+
+	$scope.shiftRight = function(i) {
+		console.log("clicked right");
+		$scope.fruit.push(this.item);
+		$scope.fruits.splice(i, 1);
+	};
+
+	$scope.shiftLeft = function(i) {
+		console.log("clicked left");
+		$scope.veggies.push(this.item);
+		$scope.vegetables.splice(i, 1);
+	};
+
 }]);
 
 //debug stuff to show the app is loading and fruit / veggies are available
